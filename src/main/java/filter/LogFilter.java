48d475eacfe97091e0e49dd925fee2c77417f9e1
MakushevEvgenii <evmakush@gmail.com>
2025-12-13 20:22:46 +0300

    доделать стилии, пересмотреть Мар

--- src/main/java/filter/LogFilter.java
+++ src/main/java/filter/LogFilter.java
@@ -5,13 +5,14 @@ import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpServletRequest;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 
 @WebFilter("/download-book")
 public class LogFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest req = (HttpServletRequest) request;
-        System.out.println("LogFilter " + req.getRequestURI());
+        System.out.println("LogFilter " + req.getRequestURI() +" program in process " + LocalDateTime.now());
         chain.doFilter(request,response);
     }
 }
